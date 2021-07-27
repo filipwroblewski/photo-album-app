fb63e3e07e871dedb25f73c2e9cb3a201c746ef0
filipwroblewski <filip.wroblewski.row@gmail.com>
2021-07-27 13:18:19 +0200

    Uploading to AWS S3 Buckets

--- photoshare/photoshare/settings.py
+++ photoshare/photoshare/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'photos.apps.PhotosConfig',
+
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -129,7 +131,17 @@ STATIC_DIRS = [
 MEDIA_ROOT = BASE_DIR / 'static/images'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+AWS_QUERYSTRING_AUTH = False
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = 'access-key'
+AWS_SECRET_ACCESS_KEY = 'secret-key'
+
+AWS_STORAGE_BUCKET_NAME = 'bucket-name'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
